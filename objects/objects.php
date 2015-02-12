bb3d50f38b8ee52feb0498a73ee4d37a0726bddf
kuroware <philiptsang018@gmail.com>
2015-02-12 03:01:01 -0500

    Update 2
    Changed objects and added in classes

--- objects/objects.php
+++ objects/objects.php
@@ -50,9 +50,48 @@ class Report extends Dashboard {
 	}
 }
 
-class Comments extends Report {
-	public function __construct($comment_id) {
+class General_Comments extends Dashboard {
+	//General class of comments to provide model functions to comments, two class of comments, inital comments and comments that are replies to comments
+	public function __construct() {
+		/*
+		Null -> Null
+		 */
+		parent::__construct();
+	}
 
+	public function get_comment_info($comment_id) {
+		/*
+		Int -> Array()
+		 */
+		$get_all_info_sql = "SELECT comment_text, user_id, date_posted, parent_id FROM comments WHERE comment_id = '$comment_id'";
+		$result_get_all_info = mysqli_query($this->dbc, $get_all_info_sql)
+		or die (mysqli_error($this->dbc));
+		return mysqli_fetch_array($result_get_all_info, mysqli_num);
+	}
+}
+
+
+class Comments extends General_Comments {
+	//Parent ID should be null
+	public function __construct($comment_id) {
+		/*
+		int -> Null
+		 */
+		parent::__construct();
+		$this->comment_id = $comment_id;
+		list($this->comment_text, $this->author_id, $this->date_posted, $this->parent_ID) = parent::get_comment_info($comment_id);
+	}
+}
+
+class Reply_Comment extends General_Comments {
+	//Parent ID should be a valid int referenced in the comment database
+	public function __construct($comment_id) {
+		/*
+		int -> Null
+		 */
+		parent::__construct();
+		$this->comment_id = $comment_id;
+		list($this->comment_text, $this->author_id, $this->date_posted, $this->parent_ID) = parent::get_comment_info($comment_id);
 	}
 }
 ?>
